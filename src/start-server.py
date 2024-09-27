b12d51d6029a31d6000f6659408ccbc12655425a
Facundo de la Plata <fdelaplata@fi.uba.ar>
2024-09-26 22:58:44 -0300

    elimino codigo repetido del server y soluciono error descarga

--- src/start-server.py
+++ src/start-server.py
@@ -1,74 +1,24 @@
 import queue
 import socket
 import threading
+from lib.stop_and_wait import *
 
-SERVER_HOST = 'localhost'
-SERVER_PORT = 12000
+SERVICE_HOST = 'localhost'
+SERVICE_PORT = 12000
+STORAGE_DIRPATH = './server_storage'
 
-CLIENT_BUFFER_SIZE = 4096
-SERVER_BUFFER_SIZE = CLIENT_BUFFER_SIZE + 1 + 4 + 8
 # Esto tiene que estar igual que en donwload.py
 # Habria que unificarlo en un solo lugar
 
 # El tamaño de p creo que hay que agrandarlo para soportar archivos mas grandes
 
-def send_data(serverSocket, clientAddress, data, p):
-	serverSocket.sendto(data, clientAddress)
-	print('Sent P', p)
-	while True:
-		try:
-			receivedData, address = serverSocket.recvfrom(CLIENT_BUFFER_SIZE)
-			i = int.from_bytes(receivedData[:1], 'big')
-			print(receivedData[1:].decode() + ' ' + str(i))
-			p += 1
-			return (address, p)
-		except socket.timeout:
-			print('Timeout ocurred sending packet', p)
-			serverSocket.sendto(data, clientAddress)
-			print('Resending P', p)
-
-def recv_data(serverSocket):
-	data, clientAddress = serverSocket.recvfrom(SERVER_BUFFER_SIZE)
-	p = int.from_bytes(data[:1], 'big')
-	print('Received P', p)
-	type = data[1:5].decode()
-	if (type == 'DONE'):
-		payload = type
-		return (payload, type, p, clientAddress)
-	payload = data[5:]
-	if (type == 'DATA'):
-		return (payload, type, p, clientAddress)
-	else:
-		return (payload.decode(), type, p, clientAddress)
-
-def send_ack(serverSocket, clientAddress, p):
-	serverSocket.sendto(p.to_bytes(1, 'big') + 'ACK'.encode(), clientAddress)
-	print('Sent ACK', p)
-
-def send_close(serverSocket, clientAddress, p):
-	tries = 0
-	data = p.to_bytes(1, 'big') + 'DONE'.encode()
-	while tries < 10:
-		try:
-			serverSocket.sendto(data, clientAddress)
-			print('Sending DONE', p)
-			receivedData, address = serverSocket.recvfrom(CLIENT_BUFFER_SIZE)
-			i = int.from_bytes(receivedData[:1], 'big')
-			print(receivedData[1:].decode() + ' ' + str(i))
-			print('Ending gracefully')
-			return
-		except socket.timeout:
-			tries += 1
-	print('Ending doubtfully')
-
 def handle_upload(server_socket, client_address, filename, payload, p):
 	# Abre o crea un archivo donde se almacenarán los datos recibidos
 	# aca se puede chequear si el header es correcto?
 	if payload != None:
-		with open('server_storage/' + filename, 'ab') as f:
-			
+		path = STORAGE_DIRPATH + '/' + filename
+		with open(path, 'ab') as f:
 			f.write(payload)  # Escribe el contenido binario en el archivo
-
 		print(f"Recibido paquete {p} de {client_address} con {len(payload)} bytes")
     
     # Enviar ACK al cliente para confirmar recepción
@@ -76,10 +26,10 @@ def handle_upload(server_socket, client_address, filename, payload, p):
 
 def handle_download(serverSocket, clientAddress, filename, p, offset):
 	done = False
-	path = 'server_storage/' + filename
+	path = STORAGE_DIRPATH + '/' + filename
 	with open(path, 'rb') as file:
 		file.seek(offset)
-		bytesRead = file.read(CLIENT_BUFFER_SIZE)
+		bytesRead = file.read(SENDER_BUFFER_SIZE)
 		if bytesRead:
 			data = p.to_bytes(1, 'big') + 'DATA'.encode() + filename.encode() + bytesRead
 			serverSocket.sendto(data, client_address)
@@ -100,11 +50,13 @@ def parse_data(data):
 	type = data[1:5].decode()
 	if (type == 'ACK1'):
 		return None, None, 'ACK1', p
-	filename = data[5:13].decode()
+	filename = None
 	print('Received P - ', p, type, filename)
 	payload = None
 	if (type == 'DATA'):
 		payload = data[13:]
+	if type == 'FILE' or type == 'DOWN':
+		filename = data[5:].decode()
 	return payload, filename, type, p
 
 # Función que maneja cada cliente de manera concurrente
@@ -114,21 +66,29 @@ def handle_client(client_address, server_socket):
 	# que no va a cambiar hasta que se cierre la conexion y el thread
     print(f"[INFO] Iniciando manejo para el cliente: {client_address}")
     i=1
-    offset = 0
     last_p_sent = 0
     done = False
+
+	#Variables que se inicializan con el primer mensaje
+    file_name = None
+    offset = 0
+
     while True:
         try:
             data = clients[client_address]["data_queue"].get(timeout=1)
 
             # Aquí se procesaría el paquete y se manejaría la lógica de Upload o Download
             payload, filename, msg_type, p = parse_data(data)
-            
+            if msg_type == 'FILE' or msg_type == 'DOWN':
+				# Es el mensaje que inicia la transferencia
+				# Se guarda el nombre del archivo
+                file_name = filename
+
             if msg_type == 'FILE' or msg_type == 'DATA':
-                handle_upload(server_socket, client_address, filename, payload, p)
+                handle_upload(server_socket, client_address, file_name, payload, p)
             elif msg_type == 'DOWN':
-                file_name = filename # medio hardcodeado pero es para poder responder el ACK
-                offset, last_p_sent, done = handle_download(server_socket, client_address, filename, last_p_sent, offset)
+                #file_name = filename # medio hardcodeado pero es para poder responder el ACK
+                offset, last_p_sent, done = handle_download(server_socket, client_address, file_name, last_p_sent, offset)
             elif msg_type == 'DONE':
 				# este done se recibe cuando se termina el upload
                 print(f"[INFO] Cliente {client_address} ha terminado la transferencia.")
@@ -161,14 +121,14 @@ clients = {}
 # Bloqueo para asegurar acceso sincronizado al diccionario de clientes
 clients_lock = threading.Lock()
 # Crear socket UDP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind((SERVICE_HOST, SERVICE_PORT))
 
-print(f"[INFO] Servidor escuchando en {SERVER_HOST}:{SERVER_PORT}")
+print(f"[INFO] Servidor escuchando en {SERVICE_HOST}:{SERVICE_PORT}")
 
 while True:
 	# Recibir datos de cualquier cliente
-	data, client_address = server_socket.recvfrom(SERVER_BUFFER_SIZE)
+	data, client_address = server_socket.recvfrom(RECEIVER_BUFFER_SIZE)
 	
 	# Bloqueo para acceso seguro al diccionario de clientes
 	with clients_lock:
--- src/upload.py
+++ src/upload.py
@@ -6,6 +6,25 @@ SERVER_PORT = 12000
 FILENAME = argv[1]
 SOURCE_FILEPATH = 'files/' + FILENAME
 
+"""
+    FILE message
+    | P | TYPE | FILENAME |
+    | 1 |  4   |    8     |
+
+    DONE message
+    | P | TYPE | FILENAME |
+    | 1 |  4   |    8     |
+    
+    ACK message
+    | P | TYPE | FILENAME |
+    | 1 |  4   |    8     |
+    
+    DATA message
+    | P | TYPE |  PAYLOAD  |
+    | 1 |  4   |   4096    |
+"""
+
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
 p = 0
