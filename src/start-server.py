244c570d4ae2a2ab6b4b76979bb6d7e289cc9c76
Santiago Marczewski <smarczewski@fi.uba.ar>
2024-09-28 19:21:29 -0300

    Add multi-threading capability

--- src/start-server.py
+++ src/start-server.py
@@ -1,6 +1,20 @@
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from lib.stop_and_wait import *
 from cli import *
+
+N_THREADS = 10
+
+def handle_connection(filename, type, p, clientAddress):
+	filepath = args.storage + '/' + filename
+	newServerSocket = socket(AF_INET, SOCK_DGRAM)
+	newServerSocket.bind((args.host, 0))
+
+	send_ack(newServerSocket, clientAddress, p)
+	if (type == 'DOWN'):
+		send_file(newServerSocket, clientAddress, filepath, p+1)
+	elif (type == 'FILE'):
+		recv_file(newServerSocket, clientAddress, filepath, type)
 
 argsparser = get_argparser(App.SERVER)
 args = get_args(argsparser, App.SERVER)
@@ -8,18 +22,12 @@ args = get_args(argsparser, App.SERVER)
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((args.host, args.port))
 
-print(args)
+with ThreadPoolExecutor(max_workers=N_THREADS) as pool:
 
-while True:
-	print('Listening for clients')
-	filename, type, p, clientAddress = recv_data(serverSocket)
-	filepath = args.storage + '/' + filename
-	newServerSocket = socket(AF_INET, SOCK_DGRAM)
-	newServerSocket.bind((args.host, args.port+1))
-	send_ack(newServerSocket, clientAddress, p)
-	if (type == 'DOWN'):
-		send_file(newServerSocket, clientAddress, filepath, p+1)
-	elif (type == 'FILE'):
-		recv_file(newServerSocket, clientAddress, filepath, type)
+	while True:
+		print('Listening for clients')
+		filename, type, p, clientAddress = recv_data(serverSocket)
+		pool.submit(handle_connection, filename, type, p, clientAddress)
+		
 
 
