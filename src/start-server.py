50159ddcd383c5b9f0a3667418989fd5573edbd7
Facundo de la Plata <fdelaplata@fi.uba.ar>
2024-10-03 17:17:00 -0300

    linter server y upload

--- src/start-server.py
+++ src/start-server.py
@@ -1,65 +1,72 @@
+"""
+Server
+"""
+from socket import socket, AF_INET, SOCK_DGRAM
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from lib.cli import *
+from lib.cli import get_argparser, get_args, App
 
 N_THREADS = 10
 
+
 def handle_connection(filename, type, p, clientAddress):
-	filepath = args.storage + '/' + filename
-	newServerSocket = socket(AF_INET, SOCK_DGRAM)
-	newServerSocket.bind((args.host, 0))
-	print("Handling connection")
-	send_ack(newServerSocket, clientAddress, p, args.verbose)
-	if (type == 'DOWN'):
-		newServerSocket.settimeout(SENDER_TIMEOUT)
+    filepath = args.storage + '/' + filename
+    newServerSocket = socket(AF_INET, SOCK_DGRAM)
+    newServerSocket.bind((args.host, 0))
+    print("Handling connection")
+    send_ack(newServerSocket, clientAddress, p, args.verbose)
+    if (type == 'DOWN'):
+        newServerSocket.settimeout(SENDER_TIMEOUT)
 
-		# Este if está porque sr recibe 0 y sw p+1
-		if args.modesr:
-			# Selective repeat
-			print("HOLAAAA2")
-			send_file(newServerSocket, clientAddress, filepath, 0, args.verbose)
-		else:
-			# Stop and wait
-			send_file(newServerSocket, clientAddress, filepath, p+1, args.verbose)
+        # Este if está porque sr recibe 0 y sw p+1
+        if args.modesr:
+            # Selective repeat
+            send_file(newServerSocket, clientAddress, filepath, 0, args.verbose)
+        else:
+            # Stop and wait
+            send_file(newServerSocket, clientAddress, filepath, p+1, args.verbose)
 
-	elif (type == 'FILE'):
-		if args.modesr:
-			# Selective repeat
-			recv_file(newServerSocket, clientAddress, filepath, type, 1, args.verbose)
-		else:
-			# Stop and wait
-			newServerSocket.settimeout(RECEIVER_TIMEOUT)
-			recv_file(newServerSocket, clientAddress, filepath, args.verbose)
+    elif type == 'FILE':
+        if args.modesr:
+            # Selective repeat
+            recv_file(newServerSocket, clientAddress, filepath, type, 1, args.verbose)
+        else:
+            # Stop and wait
+            newServerSocket.settimeout(RECEIVER_TIMEOUT)
+            recv_file(newServerSocket, clientAddress, filepath, args.verbose)
 
-	newServerSocket.close()
+    newServerSocket.close()
+
 
 argsparser = get_argparser(App.SERVER)
 args = get_args(argsparser, App.SERVER)
 
 if args.modesr:
-	from lib.selective_repeat import *
+    from lib.selective_repeat import recv_data, send_file, recv_file, send_error
+    from lib.selective_repeat import send_ack, SENDER_TIMEOUT
 else:
-	from lib.stop_and_wait import *
+    from lib.stop_and_wait import recv_data, send_file, send_error
+    from lib.stop_and_wait import recv_file, send_ack
+    from lib.stop_and_wait import RECEIVER_TIMEOUT, SENDER_TIMEOUT
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((args.host, args.port))
 
 with ThreadPoolExecutor(max_workers=N_THREADS) as pool:
 
-	while True:
-		print('Listening for clients')
+    while True:
+        print('Listening for clients')
 
-		filename, type, p, clientAddress = recv_data(serverSocket, args.verbose)
+        filename, type, p, clientAddress = recv_data(serverSocket, args.verbose)
 
-		if (type == 'DOWN'):
-			if not Path(args.storage + '/' + filename).exists():
-				err_msg = f"ERROR: The server storage doesn't contain the file {filename}".encode()
-				#end_error(serverSocket, clientAddress, p, err_msg, args.verbose)
-				continue
-		elif (type == 'FILE'):
-			if Path(args.storage + '/' + filename).exists():
-				err_msg = f"ERROR: The server storage already contains a file named {filename}".encode()
-				send_error(serverSocket, clientAddress, p, err_msg, args.verbose)
-				continue
+        if (type == 'DOWN'):
+            if not Path(args.storage + '/' + filename).exists():
+                err_msg = f"ERROR: The server storage doesn't contain the file {filename}".encode()
+                continue
+        elif (type == 'FILE'):
+            if Path(args.storage + '/' + filename).exists():
+                err_msg = f"ERROR: The server storage already contains a file named {filename}".encode()
+                send_error(serverSocket, clientAddress, p, err_msg, args.verbose)
+                continue
 
-		pool.submit(handle_connection, filename, type, p, clientAddress)
+        pool.submit(handle_connection, filename, type, p, clientAddress)
--- src/upload.py
+++ src/upload.py
@@ -1,5 +1,9 @@
-from lib.cli import *
+"""
+Upload client
+"""
 import time
+from socket import socket, AF_INET, SOCK_DGRAM
+from lib.cli import get_argparser, get_args, App
 
 start_time = time.time()
 
@@ -8,27 +12,28 @@ args = get_args(argsparser, App.CLIENT_UPLOAD)
 
 if args.modesr:
     print("MODESR ON")
-    from lib.selective_repeat import *
+    from lib.selective_repeat import establish_connection, send_file
+    from lib.selective_repeat import PACKET_NUMBER_BYTES, SENDER_TIMEOUT
 else:
-    from lib.stop_and_wait import *
+    from lib.stop_and_wait import establish_connection, send_file
+    from lib.stop_and_wait import PACKET_NUMBER_BYTES, SENDER_TIMEOUT
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(SENDER_TIMEOUT)
 p = 0
-type = 'FILE'
+type_msg = 'FILE'
 
-data = p.to_bytes(PACKET_NUMBER_BYTES, 'big') + type.encode() + args.name.encode()
+data = p.to_bytes(PACKET_NUMBER_BYTES, 'big') + type_msg.encode() + args.name.encode()
 
 try:
     serverAddress, p = establish_connection(clientSocket, (args.host, args.port), data, p, args.verbose)
-    p+=1
-except:
+    p += 1
+except Exception:
     print("FALLÓ")
     clientSocket.close()
-    exit(1)  
+    exit(1)
 
-send_file(clientSocket, serverAddress, args.src + '/' +  args.name, p, args.verbose)
+send_file(clientSocket, serverAddress, args.src + '/' + args.name, p, args.verbose)
 
 clientSocket.close()
 print("--- %s seconds ---" % (time.time() - start_time))
-
